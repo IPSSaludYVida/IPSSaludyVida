5d6d04e8726cbc7d8a37362bded16caadc02a763
Carlos J Ospina <19crxlos@gmail.com>
2024-09-29 14:23:05 -0500

    config

--- clinica/models.py
+++ clinica/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Diagnostico(models.Model):
+    codDiagnostico = models.CharField(primary_key=True, max_length=4),
+    diagnostico = models.CharField(max_length=200),
+    padre = models.IntegerField(max_length=5)
+
+    class Meta:
+        db_table = 'diagnostico'
